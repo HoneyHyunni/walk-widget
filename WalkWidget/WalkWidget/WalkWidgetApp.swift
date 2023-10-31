8ede05cd514f196b0b7672a0d0dbc41c6543cf7d
Sanghyun <dev.sanghyun@gmail.com>
2023-10-31 13:32:02 +0900

    [CHORE] create project

---
+++ WalkWidget/WalkWidget/WalkWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkWidgetApp.swift
+//  WalkWidget
+//
+//  Created by Sanghyun on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WalkWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
