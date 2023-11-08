13765de27d614222e8293d3b70f70b07e4779849
Sanghyun <dev.sanghyun@gmail.com>
2023-11-08 21:31:02 +0900

    [FEAT] add Health Widget and integrate with Shared UserDefaults

---
+++ WalkWidget/HealthWidget/HealthWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HealthWidgetBundle.swift
+//  HealthWidget
+//
+//  Created by Sanghyun on 11/8/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HealthWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HealthWidget()
+    }
+}
