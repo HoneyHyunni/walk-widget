caf295e7287d627995baf5dc45343c4e3c86fb4f
Sanghyun <dev.sanghyun@gmail.com>
2023-11-13 17:14:08 +0900

    [UPDATE] update Steps Widget view

--- WalkWidget/HealthWidget/Views/StepsView.swift
+++ WalkWidget/HealthWidget/Views/StepsView.swift
@@ -11,10 +11,17 @@ struct StepsView: View {
     var entry: StepsProvider.Entry
     
     var body: some View {
-        VStack {
-            Text("Steps")
-            Text(entry.steps.description)
-                .bold()
+        ZStack {
+            Color(.widgetBackgroundOfSteps)
+                .padding(-100)
+            VStack(alignment: .leading) {
+                Image(.steps)
+                    .resizable()
+                    .frame(width: 31, height: 31)
+                Text("오늘의 걸음수")
+                Text(entry.steps.description)
+                ProgressView(value: Double(entry.steps) / 10000.0)
+            }
         }
     }
 }
